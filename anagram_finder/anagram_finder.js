3a96dd5fbe5838fd83eaad979ce781992d42f8ab
DavidTaylor96 <david_taylor96@outlook.com>
2021-02-17 18:40:28 +0000

    couldn't finsh extensiton

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,12 @@
 const AnagramFinder = function (word) {
-
+  this.word = word.split('');
+  console.log(this.work)
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  return otherWords.filter((otherWord) => {
+    return otherWord.split('') === this.word.length
+  })
 }
 
 module.exports = AnagramFinder;
