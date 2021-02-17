acc64a17532c4c35f207b7c4753521e67d4f289a
DavidTaylor96 <david_taylor96@outlook.com>
2021-02-17 18:06:55 +0000

    isogram finshed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,12 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+  count = 1
+  return this.word.every((banana, index) => {
+    return this.word.indexOf(banana) === index
+  });
+};
 
 module.exports = IsogramFinder;
