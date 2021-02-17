ed098efb19dcb139888aa16b6635390f0d6748c6
DavidTaylor96 <david_taylor96@outlook.com>
2021-02-17 17:21:57 +0000

    phrase finshed

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase()
 }
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+  return this.alphabet.every((alphabet) => {
+    return this.phrase.includes(alphabet)
+  });
+};
 
 module.exports = PangramFinder;
